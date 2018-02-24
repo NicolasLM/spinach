9e8ea108b0b58384badfae36f68a0850b762bdf0
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-02-24 16:14:27 +0100

    Allow to send retry errors to Sentry

--- spinach/contrib/sentry.py
+++ spinach/contrib/sentry.py
@@ -3,29 +3,38 @@ from typing import Optional
 from spinach import signals
 
 
-def register_sentry(raven_client, namespace: Optional[str]=None):
+def register_sentry(raven_client, namespace: Optional[str]=None,
+                    send_retries: bool=False):
     """Register the Sentry integration.
 
-    Exceptions making jobs fail are sent to Sentry. Note that exceptions
-    resulting in the job being retried are not sent to Sentry.
+    Exceptions making jobs fail are sent to Sentry.
 
     :param raven_client: configured Raven client used to sent errors to Sentry
     :param namespace: optionally only register the Sentry integration for a
-                      particular Spinach :class:`Engine`.
+           particular Spinach :class:`Engine`
+    :param send_retries: whether to also send to Sentry exceptions resulting
+           in a job being retried
     """
 
     @signals.job_started.connect_via(namespace)
-    def job_started(*args, job=None, **kwargs):
+    def job_started(namespace, job, **kwargs):
         raven_client.context.activate()
         raven_client.transaction.push(job.task_name)
 
     @signals.job_finished.connect_via(namespace)
-    def job_finished(*args, job=None, **kwargs):
+    def job_finished(namespace, job, **kwargs):
         raven_client.transaction.pop(job.task_name)
         raven_client.context.clear()
 
     @signals.job_failed.connect_via(namespace)
-    def job_failed(*args, job=None, **kwargs):
+    def job_failed(namespace, job, **kwargs):
         raven_client.captureException(
             extra={attr: getattr(job, attr) for attr in job.__slots__}
         )
+
+    if send_retries:
+        @signals.job_schedule_retry.connect_via(namespace)
+        def job_schedule_retry(namespace, job, **kwargs):
+            raven_client.captureException(
+                extra={attr: getattr(job, attr) for attr in job.__slots__}
+            )
--- tests/contrib/test_sentry.py
+++ tests/contrib/test_sentry.py
@@ -11,13 +11,17 @@ from spinach.contrib.sentry import register_sentry
 def spin():
     tasks = Tasks()
 
+    @tasks.task(name='success')
+    def success():
+        return
+
     @tasks.task(name='fail')
     def fail():
         raise RuntimeError('failing task')
 
-    @tasks.task(name='success')
-    def success():
-        return
+    @tasks.task(name='retry', max_retries=1)
+    def retry():
+        raise RuntimeError('retrying task')
 
     s = Engine(MemoryBroker(), namespace='tests')
     s.attach_tasks(tasks)
@@ -31,7 +35,7 @@ def test_sentry(spin):
     register_sentry(raven_client, spin.namespace)
 
     spin.schedule('success')
-    time.sleep(1)
+    time.sleep(0.1)
     raven_client.context.activate.assert_called_once_with()
     raven_client.transaction.push.assert_called_once_with('success')
     raven_client.transaction.pop.assert_called_once_with('success')
@@ -40,9 +44,23 @@ def test_sentry(spin):
 
     raven_client.reset_mock()
     spin.schedule('fail')
-    time.sleep(1)
+    time.sleep(0.1)
     raven_client.context.activate.assert_called_once_with()
     raven_client.transaction.push.assert_called_once_with('fail')
     raven_client.transaction.pop.assert_called_once_with('fail')
     raven_client.context.clear.assert_called_once_with()
+    assert raven_client.captureException.call_count == 1
+
+    raven_client.reset_mock()
+    spin.schedule('retry')
+    time.sleep(0.1)
+    assert raven_client.captureException.call_count == 0
+
+
+def test_sentry_sending_retries(spin):
+    raven_client = Mock()
+    register_sentry(raven_client, spin.namespace, send_retries=True)
+    raven_client.reset_mock()
+    spin.schedule('retry')
+    time.sleep(0.1)
     assert raven_client.captureException.call_count == 1

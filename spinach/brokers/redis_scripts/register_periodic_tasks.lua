9994f71e28b924edca27188e2da2ff4a2043992b
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-08-30 14:58:57 +0200

    Reset periodic task score when periodicity is changed
    When the periodicity of a periodic task was changed, it has to wait
    until the next planed execution before being changed.
    
    Thus switching from 1 week periodicity to 1 second meant waiting one
    full week before seeing the change reflected.
    
    This commit fixes this issue by completely reseting a periodic task
    when its periodicity changes.

--- spinach/brokers/redis_scripts/register_periodic_tasks.lua
+++ spinach/brokers/redis_scripts/register_periodic_tasks.lua
@@ -20,8 +20,19 @@ for i=5, #ARGV do
     new_task_names[task["name"]] = true
 
     if redis.call('hexists', periodic_tasks_hash, task["name"]) == 0 then
+        -- the periodic task is new, add it to the queue
         redis.call('zadd', periodic_tasks_queue, next_event_time, task["name"])
+    else
+        local existing_task_json = redis.call('hget', periodic_tasks_hash, task["name"])
+        local existing_task = cjson.decode(existing_task_json)
+        if existing_task["periodicity"] ~= task["periodicity"] then
+            -- the periodic task already existed but the periodicity changed
+            -- so it is reset
+            redis.call('zadd', periodic_tasks_queue, next_event_time, task["name"])
+        end
     end
+
+    -- unconditionnally override the task in the hash
     redis.call('hset', periodic_tasks_hash, task["name"], task_json)
 end
 
@@ -30,6 +41,7 @@ for i=1, #old_task_names do
     local old_task_name = old_task_names[i]
     if not contains(new_task_names, old_task_name) then
         redis.call('hdel', periodic_tasks_hash, old_task_name)
+        redis.call('zrem', periodic_tasks_queue, old_task_name)
     end
 end
 

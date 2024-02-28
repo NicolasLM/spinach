e7a48134fff1dda0dc2593c3e00237dba7107b4f
Nicolas Simonds <nisimond@cisco.com>
2024-02-28 10:16:22 -0800

    Modernize Python tooling
    Move all configs into `pyproject.toml`, switch test/packaging to
    use Hatch, and do all the linting with Ruff.
    
    Fix issues that Ruff turned up, and annotate/ignore the pieces that
    it got wrong.
    
    Fixes: Issue #33

--- spinach/__init__.py
+++ spinach/__init__.py
@@ -1,8 +1,5 @@
 from .brokers.memory import MemoryBroker
 from .brokers.redis import RedisBroker
-from .const import VERSION
 from .engine import Engine
 from .task import Tasks, Batch, RetryException, AbortException
 from .worker import ThreadWorkers, AsyncioWorkers
-
-__version__ = VERSION
